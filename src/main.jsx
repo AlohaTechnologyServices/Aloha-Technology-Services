2b54ea145ad62007f6f101a5c7a525849f48f69d
AlohaTechnologyServices <alohatechnologyservice@gmail.com>
2026-08-11 19:36:54 -1000

    Load inspection page refinements

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./AppWithUpdates";
 import "./index.css";
+import "./inspection-page-refinements.css";
+import "./inspection-page-refinements.js";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>

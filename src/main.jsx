69ea991bfbbb935f0f0a98c8bca5d39f9e7283da
AlohaTechnologyServices <alohatechnologyservice@gmail.com>
2026-08-11 19:37:56 -1000

    Trigger final inspection refinements deployment

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,7 @@ import "./index.css";
 import "./inspection-page-refinements.css";
 import "./inspection-page-refinements.js";
 
+// Inspection-page refinements are loaded globally so service-detail updates remain consistent during SPA navigation.
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />

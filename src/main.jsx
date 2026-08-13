4f3f18de604dd6ca3a09840d8f33ed91c008b58e
AlohaTechnologyServices <alohatechnologyservice@gmail.com>
2026-08-12 22:19:12 -1000

    Load application development copy refinement

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./inspection/pricingOverrides";
+import "./applicationDevelopmentCopy";
 import App from "./AppWithUpdates";
 import "./index.css";
 

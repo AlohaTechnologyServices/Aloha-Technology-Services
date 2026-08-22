9f5411c0b985e98ff944f1d224d17228fad1505c
AlohaTechnologyServices <alohatechnologyservice@gmail.com>
2026-08-21 15:23:51 -1000

    Load homepage pricing placement adjustment

--- src/main.jsx
+++ src/main.jsx
@@ -2,8 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./inspection/pricingOverrides";
 import "./applicationDevelopmentCopy";
+import "./homepagePricingAdjustments";
 import App from "./AppWithUpdates";
 import "./index.css";
+import "./homepage-pricing-adjustments.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
